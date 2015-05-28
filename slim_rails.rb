6b857fc7004f664a281addfa030f2d33db775036
Bodacious <gavin@katanacode.com>
2015-05-28 17:07:12 +0100

    Updated Application template

--- slim_rails.rb
+++ slim_rails.rb
@@ -1,3 +1,7 @@
 puts "Configuring application for Slim rails"
 
 gem "slim-rails"
+gem 'compass-rails'
+
+mv "app/assets/stylesheets/application.css app/assets/stylesheets/application.sass"
+run "echo @import \"base\" >> app/assets/stylesheets/application.sass"
